fa574882e9d6b3c73933f17693b9a0602e108896
Jsaulbautista <150806077+Jsaulbautista@users.noreply.github.com>
2023-11-14 09:08:50 -0500

    Update HelloWorld.java
    se añadio un nuevo comentario

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String[]args) {
-		System.out.println("hello hello ");
+		System.out.println("hello hello");
+		System.out.println("como vas?")
 		System.out.println("hola mundo");
 	}
 }
