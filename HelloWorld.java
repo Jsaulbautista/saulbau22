676ba5d4a5f815101e88bf81913eda90c9938d95
Jsaulbautista <150806077+Jsaulbautista@users.noreply.github.com>
2023-11-14 09:12:25 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[]args) {
-		System.out.println("hello hello");
+		System.out.println("hello miamor");
 		System.out.println("como vas?")
 		System.out.println("hola mundo");
 	}
